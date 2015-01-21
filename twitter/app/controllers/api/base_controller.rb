3a9cfe3d441eb0b8bbe8db8c609fd5f24da5cf20
Alejandro Perez <alexgt9@gmail.com>
2015-01-21 16:21:48 +0100

    Added support for xml format

--- twitter/app/controllers/api/base_controller.rb
+++ twitter/app/controllers/api/base_controller.rb
@@ -2,7 +2,7 @@ module Api
   class BaseController < ApplicationController
     protect_from_forgery with: :null_session
     before_action :set_resource, only: [:destroy, :show, :update]
-    respond_to :json
+    respond_to :json, :xml
 
     # POST /api/{plural_resource_name}
 	def create
